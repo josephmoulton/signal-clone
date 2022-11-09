4f07ce274bc2664e06bf14423cee99ffcbfb6862
Joe <josephlewismoulton@gmail.com>
2022-11-09 13:43:41 +0000

    Login page and register page completed, functionality not yet implemented

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyApFmJJyW1X4V6FT7OYsW51lIFN4zOlqXo",
+  authDomain: "signal-clone-9a9b2.firebaseapp.com",
+  projectId: "signal-clone-9a9b2",
+  storageBucket: "signal-clone-9a9b2.appspot.com",
+  messagingSenderId: "182737611546",
+  appId: "1:182737611546:web:b9c435ff66869711e15aa3",
+};
